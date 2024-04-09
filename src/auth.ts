0be968d3fdfadf7f5071fb311209d0a73f298e9f
Mateusz Dionizy <62315827+deronek@users.noreply.github.com>
2024-04-09 21:00:36 +0200

    Keep Auth0 only for demonstration

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,6 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/sveltekit/providers/github"
-import Google from "@auth/sveltekit/providers/google"
-import Facebook from "@auth/sveltekit/providers/facebook"
 import Auth0 from "@auth/sveltekit/providers/auth0"
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-  providers: [GitHub, Google, Facebook, Auth0],
+  providers: [Auth0],
 })
