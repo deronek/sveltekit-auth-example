230836cb600db61db0ac81619a0caac83296b1a1
deronek <dionek3@gmail.com>
2024-04-10 17:30:04 +0200

    Add reproduction

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -6,25 +6,25 @@
 <header>
   <div class="signedInStatus">
     <div class="nojs-show loaded">
-      <img
-        alt="User avatar"
-        src={$page.data?.session?.user?.image ??
-          "https://source.boringavatars.com/marble/120"}
-        class="avatar"
-      />
-      {#if $page.data.session}
-        <span class="signedInText">
-          {$page.data.session.user?.email ?? $page.data.session.user?.name}
-        </span>
-        <SignOut>
-          <div slot="submitButton" class="buttonPrimary">Sign out</div>
-        </SignOut>
-      {:else}
-        <span class="notSignedInText">You are not signed in</span>
-        <SignIn>
-          <div slot="submitButton" class="buttonPrimary">Sign in</div>
-        </SignIn>
-      {/if}
+      {#await $page.data.sessionLazy}
+      Loading...
+      {:then session}
+        {#if session}
+          <span class="signedInText">
+            {session.user?.email ?? session.user?.name}
+          </span>
+          <SignOut>
+            <div slot="submitButton" class="buttonPrimary">Sign out</div>
+          </SignOut>
+        {:else}
+          <span class="notSignedInText">You are not signed in</span>
+          <SignIn>
+            <div slot="submitButton" class="buttonPrimary">Sign in</div>
+          </SignIn>
+        {/if}
+      {:catch}
+        Could not load user data
+      {/await}
     </div>
   </div>
   <nav>
@@ -73,15 +73,6 @@
   .signedInText {
     padding-top: 0rem;
     left: 4.6rem;
-  }
-  .avatar {
-    border-radius: 2rem;
-    float: left;
-    height: 2.8rem;
-    width: 2.8rem;
-    background-color: white;
-    background-size: cover;
-    background-repeat: no-repeat;
   }
   .buttonPrimary {
     font-weight: 500;
